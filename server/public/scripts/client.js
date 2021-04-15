c1a0716f98d17e1a3846c161b694808698da9fa6
Johnny Keple <Johnny.Keple@gmail.com>
2021-04-15 14:38:21 -0500

    Finished redner fucntion

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,27 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!")
+    addNumbers();
+}
+
+function addNumbers() {
+  let newNumbers = {
+    playerOne: $('#player-one').val(),
+    playerTwo: $('#player-two').val()
+  }
+  console.log('testing numbers', newNumbers);
+  
+}
+
+function render(numbersArray) {
+    $('#numberGuesser').empty();
+    for (let item of numbersArray) {
+      console.log(`${item.playerOne}, ${item.playerTwo}`);
+      $('#numberGuesser').append(`
+          <div class="displayedGuess">
+              <p>${item.playerOne}</p>
+              <p>${item.playerTwo}</p>
+          </div>
+      `)
+    }
 }
